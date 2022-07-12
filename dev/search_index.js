f274db5f87d0b1681131c970cb2786657ec5581e
Documenter.jl <documenter@juliadocs.github.io>
2022-07-12 13:57:21 +0000

    build based on e22b38a

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = KMCSurrogates","category":"page"},{"location":"#KMCSurrogates","page":"Home","title":"KMCSurrogates","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for KMCSurrogates.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [KMCSurrogates]","category":"page"}]
+}
